8d708172e050a595bccf75163fb75edfb7e48708
Matheus Gomes <matheusprgc@gmail.com>
2024-09-06 21:41:29 -0300

    feat: endpoint de cadastro do usuário e interações.

--- Index.js
+++ Index.js
@@ -1,24 +1,40 @@
 import express from 'express';
 import db from './Data/DbContext.js';
 
-const server = express();
+const app = express();
+const port = 3000;
 
-// Tentar conexão com o banco de dados antes de iniciar o servidor
-db.any('SELECT 1 + 1 AS result')
-    .then(result => {
-        console.log('Conexão ao banco de dados bem-sucedida:', result);
+// Rotas
+app.post('/usuarios', async (req, res) =>{
+    const {nome, alegre, itermediario, triste} = req.body;
 
-        // Iniciar o servidor somente após a conexão bem-sucedida
-        server.listen(3000, () => {
-            console.log('Servidor rodando na porta 3000');
-        });
-    })
-    .catch(error => {
-        console.error('Erro ao conectar ao banco de dados:', error);
-        process.exit(1); // Finaliza o processo caso a conexão falhe
-    });
+    if(nome === undefined || alegre === undefined || itermediario === undefined
+        || triste === undefined)
+        {
+            return res.status(400).json({error: 'Todos os campos são necessários: nome, alegre, intermediario, triste'});
+        }
 
-// Rota de teste
-server.get('/', (req, res) => {
+        const query = `
+        INSERT INTO usuarios (nome, alegre, intermediario, triste)
+        VALUES (@1, @2, @3, @4)
+        RETURNING *;
+    `;
+
+    try{
+        const result = await db.one(query, [nome, alegre, intermediario, triste]);
+        res.status(201).json({message: 'Usuário criado com sucesso!', user: result});
+    }
+    catch(error)
+    {
+        console.error('Erro ao criar o usuário:', error);
+        res.status(500).json({error: 'Erro ao cria o usuário'});
+    }
+});
+
+app.get('/', (req, res) => {
     return res.json({ message: 'Teste ok' });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
