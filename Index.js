1b9235e882b64f31ed9b8bec0f5f69d55f3b0001
Matheus Gomes <matheusprgc@gmail.com>
2024-09-06 22:08:20 -0300

    refactor: Melhoria no endpoint post usuários.

--- Index.js
+++ Index.js
@@ -4,35 +4,41 @@ import db from './Data/DbContext.js';
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // Middleware para parsing JSON
+
 // Rotas
-app.post('/usuarios', async (req, res) =>{
-    const {nome, alegre, itermediario, triste} = req.body;
+app.post('/usuarios', async (req, res) => {
+    const { nome, alegre, intermediario, triste } = req.body;
 
-    if(nome === undefined || alegre === undefined || itermediario === undefined
-        || triste === undefined)
-        {
-            return res.status(400).json({error: 'Todos os campos são necessários: nome, alegre, intermediario, triste'});
-        }
+    if (nome === undefined || alegre === undefined || intermediario === undefined || triste === undefined) {
+        return res.status(400).json({ error: 'Todos os campos são necessários: nome, alegre, intermediario, triste' });
+    }
 
-        const query = `
+    const query = `
         INSERT INTO usuarios (nome, alegre, intermediario, triste)
-        VALUES (@1, @2, @3, @4)
+        VALUES ($1, $2, $3, $4)
         RETURNING *;
     `;
 
-    try{
+    try {
         const result = await db.one(query, [nome, alegre, intermediario, triste]);
-        res.status(201).json({message: 'Usuário criado com sucesso!', user: result});
-    }
-    catch(error)
-    {
+        res.status(201).json({ message: 'Usuário criado com sucesso!', user: result });
+    } catch (error) {
         console.error('Erro ao criar o usuário:', error);
-        res.status(500).json({error: 'Erro ao cria o usuário'});
+        res.status(500).json({ error: 'Erro ao criar o usuário.' });
     }
 });
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Teste ok' });
+app.get('/usuarios', async (req, res) => {
+    const query = 'SELECT * FROM usuarios';
+
+    try {
+        const result = await db.any(query);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error('Erro ao buscar os usuários:', error);
+        res.status(500).json({ error: 'Erro ao buscar os usuários.' });
+    }
 });
 
 app.listen(port, () => {
